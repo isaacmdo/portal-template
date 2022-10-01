0fc9d234df61d407f08efc15852211f92082a987
Isaac <isaacestudo1999@gmail.com>
2022-09-30 23:09:31 -0300

    Migration 6.0

--- Template/Program.cs
+++ Template/Program.cs
@@ -1,26 +1,126 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Template.Application.AutoMapper;
+using Template.CrossCutting.Auth.Models;
+using Template.CrossCutting.Auth.Providers;
+using Template.CrossCutting.ExceptionHandler.Providers;
+using Template.CrossCutting.IoC;
+using Template.CrossCutting.Notification.ViewModels;
+using Template.CrossCutting.Swagger;
+using Template.Data.Context;
 
-namespace Template
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddDbContext<PortalContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+              , builder =>
+              {
+                  builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+              }
+              ).EnableSensitiveDataLogging()
+          );
+
+
+
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.AddPolicy("CorsPolicy",
+    builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddSingleton(builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
+NativeInjector.RegisterServices(builder.Services);
+builder.Services.AddSwaggerConfiguration();
+builder.Services.AddCustomJWTConfiguration();
+builder.Services.AddAutoMapper(typeof(AutoMapperSetup));
+
+builder.Services.AddControllersWithViews();
+// In production, the Angular files will be served from this directory
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist";
+});
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseExceptionHandlerMiddleware();
+app.UseSwaggerConfiguration();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseStaticFiles();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
+
+app.UseRouting();
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+});
+
+app.UseSpa(spa =>
+{
+    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+    // see https://go.microsoft.com/fwlink/?linkid=864501
+
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseAngularCliServer(npmScript: "start");
+    }
+});
+
+
+app.MapControllers();
+app.Run();
